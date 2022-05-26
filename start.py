24c9ec92d3c2589bf438c3128932fe1b4986e50f
Preetam <preetam_ghosh_1987@yahoo.co.in>
2022-05-26 18:19:37 +0530

    Initial python commit

---
+++ start.py
@@ -0,0 +1,17 @@
+
+# you need to declare a function before calling it
+
+def a():
+    listb = ["hello", "world"]
+    
+    # prints list
+    print(listb)
+    
+    # finds length of the list
+    print(len(listb))
+
+    
+
+
+
+a()
